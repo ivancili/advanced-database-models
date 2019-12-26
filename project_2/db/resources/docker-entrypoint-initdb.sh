185e4b19f2ab75b0fb457e07a63d7e933d6d3379
ivancili <ivan.ilic2@outlook.com>
2019-12-26 23:18:28 +0100

    Added database resources

---
+++ project_2/db/resources/docker-entrypoint-initdb.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+import_json_to_db () {
+
+	json_file=$1
+	
+	mongoimport \
+		-u $MONGO_INITDB_ROOT_USERNAME \
+		-p $MONGO_INITDB_ROOT_PASSWORD \
+		--authenticationDatabase admin \
+		-d nmbp \
+		-c $(basename -s .json $json_file)\
+		--type json \
+		--file $json_file
+}
+
+
+for file in $(find /docker-entrypoint-initdb.d/ -type f -name '*json')
+do
+	import_json_to_db $file
+done
