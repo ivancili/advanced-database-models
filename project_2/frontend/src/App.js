08344ebb73960e4475351d63ca9dd28bce0d798f
ivancili <ivan.ilic2@outlook.com>
2019-12-28 14:36:54 +0100

    Created reactJS app

---
+++ project_2/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Edit src/App.js and save to reload.
+          Edit src/App.js and save to reload.
+      </header>
+    </div>
+  );
+}
+
+export default App;
