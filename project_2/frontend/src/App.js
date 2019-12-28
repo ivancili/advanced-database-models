1caf90432ac27d53394e258e9497c2e72b0132d5
ivancili <ivan.ilic2@outlook.com>
2019-12-28 17:25:44 +0100

    Header

--- project_2/frontend/src/App.js
+++ project_2/frontend/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ArticleList from './components/ArticleList';
+import {Header} from "semantic-ui-react";
 
-function App() {
-    return (
-        <div className='App'>
-            <ArticleList num_articles={10}/>
-        </div>
-    );
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className='App'>
+                <Header
+                    dividing
+                    size='huge'
+                    content='News portal'
+                    style={{textAlign: 'center'}}
+                />
+                <ArticleList num_articles={10}/>
+            </div>
+        );
+    }
+
 }
 
 export default App;
