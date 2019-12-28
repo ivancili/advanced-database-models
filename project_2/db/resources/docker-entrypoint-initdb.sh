3efcea5cea53c1f8cec48e7ef40e6d412e331e09
ivancili <ivan.ilic2@outlook.com>
2019-12-28 10:27:19 +0100

    Create index in db

--- project_2/db/resources/docker-entrypoint-initdb.sh
+++ project_2/db/resources/docker-entrypoint-initdb.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
-import_json_to_db () {
+import_json_to_db() {
 
-	json_file=$1
-	
-	mongoimport \
-		-u $MONGO_INITDB_ROOT_USERNAME \
-		-p $MONGO_INITDB_ROOT_PASSWORD \
-		--authenticationDatabase admin \
-		-d nmbp \
-		-c $(basename -s .json $json_file)\
-		--type json \
-		--file $json_file
+  json_file=$1
+
+  mongoimport \
+    -u $MONGO_INITDB_ROOT_USERNAME \
+    -p $MONGO_INITDB_ROOT_PASSWORD \
+    --authenticationDatabase admin \
+    -d nmbp \
+    -c $(basename -s .json $json_file) \
+    --type json \
+    --file $json_file
 }
 
-
-for file in $(find /docker-entrypoint-initdb.d/ -type f -name '*json')
-do
-	import_json_to_db $file
+for file in $(find /docker-entrypoint-initdb.d/ -type f -name '*json'); do
+  import_json_to_db $file
 done
+
+mongo \
+  -u $MONGO_INITDB_ROOT_USERNAME \
+  -p $MONGO_INITDB_ROOT_PASSWORD \
+  --authenticationDatabase admin \
+  --eval 'db.news.createIndex({date: -1})' \
+  nmbp
